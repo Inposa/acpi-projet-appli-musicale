328b25e9defd3a8140aed31a2866eba85503961d
Theo Armand <theoarmand@hotmail.fr>
2020-02-27 15:46:08 +0100

    ajout des objets et controllers

---
+++ src/controller/Morceau_Controller.java
@@ -0,0 +1,6 @@
+package controller;
+
+import object.Morceau;
+
+public class Morceau_Controller extends Morceau {
+}
---
+++ src/controller/Playlist_Controller.java
@@ -0,0 +1,6 @@
+package controller;
+
+import object.Playlist;
+
+public class Playlist_Controller extends Playlist {
+}
