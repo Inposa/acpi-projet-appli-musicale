fa5ca2553487963d14019e1aad034b939f71e705
diazt <thomas.diaz@etu.umontpellier.fr>
2020-02-27 17:36:43 +0100

    ajout fenetre creation de playlist

---
+++ src/main/java/fr/umontpellier/etu/musidex/controller/ControllerCreation.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.umontpellier.etu.musidex.controller;
+
+import java.util.ArrayList;
+import fr.umontpellier.etu.musidex.model.I_Morceau;
+import fr.umontpellier.etu.musidex.model.Morceau;
+
+import java.util.List;
+
+/**
+ *
+ * @author diazt
+ */
+public class ControllerCreation {
+    
+    
+    public ArrayList<I_Morceau> getBibliotheque() {
+        I_Morceau m1 = new Morceau();
+        I_Morceau m2 = new Morceau();
+        I_Morceau m3 = new Morceau();
+        m1.setNom("René la Taupe");
+        m2.setNom("Crazy Frog");
+        m3.setNom("Générique de Pokémon Saison 1");
+        ArrayList<I_Morceau> lesMorceaux = new ArrayList<I_Morceau>();
+        lesMorceaux.add(m1);
+        lesMorceaux.add(m2);
+        lesMorceaux.add(m3);
+        return lesMorceaux;
+    }
+    
+
+}
--- src/main/java/fr/umontpellier/etu/musidex/model/Morceau.java
+++ src/main/java/fr/umontpellier/etu/musidex/model/Morceau.java
@@ -1,5 +1,20 @@
 package fr.umontpellier.etu.musidex.model;
 
-public class Morceau {
+public class Morceau implements I_Morceau{
+
+    String nom;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    @Override
+    public String toString() {
+        return nom;
+    }
 
 }
