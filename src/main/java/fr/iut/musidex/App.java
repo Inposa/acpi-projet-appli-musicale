da84d86d5314c1f92f2027e39af715cbc8ccea4d
Julien Faure <julien.faure02@etu-umontpellier.fr>
2020-02-27 16:49:46 +0100

    Vues ajoutées

--- src/main/java/fr/iut/musidex/App.java
+++ src/main/java/fr/iut/musidex/App.java
@@ -1,5 +1,7 @@
 package fr.iut.musidex;
 
+import fr.iut.musidex.view.HomeWindow;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        new HomeWindow();
     }
 }
