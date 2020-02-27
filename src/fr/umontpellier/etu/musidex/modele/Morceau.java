c718cfedb3cc5ecea38e855495b891acaf8810cb
pechh <pechh@K143-6.dep-info.iutmontp.univ-montp2.fr>
2020-02-27 17:27:08 +0100

    Ecriture Morceaux

--- src/fr/umontpellier/etu/musidex/modele/Morceau.java
+++ src/fr/umontpellier/etu/musidex/modele/Morceau.java
@@ -25,11 +25,9 @@ public class Morceau implements I_Morceau {
 		
 		this.lienYT = null;
 	}
-	
 	public Morceau(String titre, String interprete, Tonalite tonalite) {
 		this(titre, interprete, tonalite, 0);
 	}
-		
 	public Morceau(String titre, String interprete) {
 		this(titre, interprete, Tonalite.C);
 	}
@@ -38,7 +36,6 @@ public class Morceau implements I_Morceau {
 	public String getNom() {
 		return this.titre;
 	}
-
 	@Override
 	public void setNom(String nom) {
 		this.titre = nom;		
@@ -48,7 +45,6 @@ public class Morceau implements I_Morceau {
 	public String getInterprete() {
 		return interprete;
 	}
-
 	@Override
 	public void setInterprete(String interprete) {
 		this.interprete = interprete;
@@ -58,7 +54,6 @@ public class Morceau implements I_Morceau {
 	public Tonalite getTonalite() {
 		return this.tonalite;
 	}
-
 	@Override
 	public void setTonalite(Tonalite tonalite) {
 		this.tonalite = tonalite;
@@ -69,18 +64,16 @@ public class Morceau implements I_Morceau {
 	public float getDuree() {
 		return this.duree;
 	}
-
 	@Override
 	public void setDuree(float duree) {
 		this.duree = duree;
-		
 	}
 
-	/*@Override
-	public String toString() {
-		return "Morceau: " + this.titre + "| Interprète:" + this.interprete + "| Tonalité:" + this.tonalite;
-	}*/
-
-	
+	public String getLienYT() {
+		return lienYT;
+	}
+	public void setLienYT(String lienYT) {
+		this.lienYT = lienYT;
+	}
 
 }
