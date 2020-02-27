26b63a94065eea5ac01b7ab7de6b97ac9a476a8a
pechh <pechh@K143-6.dep-info.iutmontp.univ-montp2.fr>
2020-02-27 16:00:36 +0100

    Création de enum Tonalité et Morceau

--- src/fr/iut/entities/Morceau.java
+++ src/fr/iut/entities/Morceau.java
@@ -3,4 +3,13 @@ package fr.iut.entities;
 public class Morceau {
 	private String titre;
 	private String interprete;
+	
+	private String tonalite;
+	
+	public Morceau(String titre, String interprete) {
+		
+		
+	}
+	
+	
 }
