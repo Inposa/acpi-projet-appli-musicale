eab9000d3faa91dc93cc8740d4ae59b2a05dfdb1
AltierR <romain.altier@etu.umontpellier.fr>
2020-03-04 14:27:08 +0100

    suppression des controlleurs inutiles

--- src/main/java/fr/umontpellier/etu/musidex/controller/MorceauController.java
+++
@@ -1,6 +0,0 @@
-package fr.umontpellier.etu.musidex.controller;
-
-import fr.umontpellier.etu.musidex.model.Morceau;
-
-public class MorceauController extends Morceau {
-}
--- src/main/java/fr/umontpellier/etu/musidex/controller/PlaylistController.java
+++
@@ -1,6 +0,0 @@
-package fr.umontpellier.etu.musidex.controller;
-
-import fr.umontpellier.etu.musidex.model.Playlist;
-
-public class PlaylistController extends Playlist {
-}
