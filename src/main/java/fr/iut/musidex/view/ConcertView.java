b0a4beaf0873ad3dde39745f69be4c21910657aa
TestNRGT <test@test.test>
2020-03-05 09:41:21 +0100

    init concert mode

--- src/main/java/fr/iut/musidex/view/ConcertView.java
+++ src/main/java/fr/iut/musidex/view/ConcertView.java
@@ -10,22 +10,23 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class ConcertView extends javax.swing.JFrame {
-
+    private ConcertListMorceauxView listMorceaux;
+    private ConcertUniteMorceauView uniteMorceaux;
     
     public ConcertView() {
         initComponents();
-        ConcertListMorceauxView morc = new ConcertListMorceauxView(1);
-        ConcertUniteMorceauView unt = new ConcertUniteMorceauView();
-        tabPanel.addTab("Liste", morc);
-        tabPanel.addTab("Unité", unt);
+        listMorceaux = new ConcertListMorceauxView(1);
+        uniteMorceaux = new ConcertUniteMorceauView();
+        tabPanel.addTab("Liste", listMorceaux);
+        tabPanel.addTab("Unité", uniteMorceaux);
         
         JList<String> listMrcx = ((ConcertListMorceauxView) tabPanel.getComponentAt(0)).getList();
+        listMrcx.setSelectedIndex(0);
         listMrcx.addMouseListener(new MouseAdapter(){
             public void mouseClicked(MouseEvent evt) {
-               
                 if (evt.getClickCount() == 2) {
-                int index = listMrcx.locationToIndex(evt.getPoint());
-                tabPanel.setSelectedIndex(1);               
+                    int index = listMrcx.locationToIndex(evt.getPoint());
+                    tabPanel.setSelectedIndex(1);
                 } 
             }
         });
@@ -34,30 +35,29 @@ public class ConcertView extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jScrollPane2 = new javax.swing.JScrollPane();
+        slp = new javax.swing.JScrollPane();
         tabPanel = new javax.swing.JTabbedPane();
 
         setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         setResizable(false);
 
-        jScrollPane2.setMinimumSize(new java.awt.Dimension(500, 700));
-        jScrollPane2.setViewportView(tabPanel);
+        slp.setMinimumSize(new java.awt.Dimension(500, 700));
+        slp.setViewportView(tabPanel);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(slp, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 700, Short.MAX_VALUE)
+            .addComponent(slp, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 700, Short.MAX_VALUE)
         );
-
         pack();
-    }// </editor-fold>//GEN-END:initComponents
+    }
 
 
-    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane slp;
     private javax.swing.JTabbedPane tabPanel;
 }
